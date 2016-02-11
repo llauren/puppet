a3b883095ed0b5218b278d30bf7f17946812b822
Robin Laurén <llauren@gmail.com>
2016-02-11 20:55:36 +0200

    Unattended-upgrades configs made explicit
    Apply all upgrades and reboot if needed

--- modules/baseline/manifests/apt.pp
+++ modules/baseline/manifests/apt.pp
@@ -10,18 +10,18 @@ class baseline::apt {
   }
 
 # This may or may not be magically applied by pkg unattended upgrades :/
-#  file_line { 'apt-upgrade':
-#    path => '/etc/apt/apt.conf.d/50unattended-upgrades',
-#    match => '//\t"\$\{distro_id\}:\$\{distro_codename\}-updates";',
-#    line => '\t"\$\{distro_id\}:\$\{distro_codename\}-updates";',
-#    require => Package['unattened-upgrades'],
-#  }
+  file_line { 'apt-upgrade':
+    path => '/etc/apt/apt.conf.d/50unattended-upgrades',
+    match => '//\t"\$\{distro_id\}:\$\{distro_codename\}-updates";',
+    line => '\t"\$\{distro_id\}:\$\{distro_codename\}-updates";',
+    require => Package['unattened-upgrades'],
+  }
 
-#  file_line { 'apt-reboot':
-#    path => '/etc/apt/apt.conf.d/50unattended-upgrades',
-#    match => '[/\w]*Unattended-Upgrade::Automatic-Reboot "false";',
-#    line => '\tUnattended-Upgrade::Automatic-Reboot "true";'
-#    require => Package['unattened-upgrades'],
-#  }
+  file_line { 'apt-reboot':
+    path => '/etc/apt/apt.conf.d/50unattended-upgrades',
+    match => '[/\w]*Unattended-Upgrade::Automatic-Reboot "false";',
+    line => '\tUnattended-Upgrade::Automatic-Reboot "true";'
+    require => Package['unattened-upgrades'],
+  }
 
 }
